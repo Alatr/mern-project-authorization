96129785afc8fb8a7800b3e5c8047a5388a896c2
Dmytro Lukianov <dimalukianovweb@gmail.com>
2021-12-11 18:47:29 +0200

    cleanup trash

--- backend/app.ts
+++ backend/app.ts
@@ -3,35 +3,33 @@ import { UserController } from "./users/controller";
 import { ILogger } from "./services/logger";
 import * as dotenv from "dotenv";
 dotenv.config();
-import express, { NextFunction, Request, Response, Express } from "express";
-// // import cors from 'cors';
-// // import cookieParser from 'cookie-parser';
-// // import mongoose from 'mongoose';
-// // import router from './router/index.js';
+import express, { Express } from "express";
+
+// import cors from 'cors';
+// import cookieParser from 'cookie-parser';
+// import mongoose from 'mongoose';
+// import router from './router/index.js';
 // import errorMiddleware from './middlewares/error-middleware.js';
 
-const PORT = process.env.PORT || 5002;
+const PORT = Number(process.env.PORT) || 5002;
 // const app = express();
 
-// // app.use(express.json());
-// // app.use(cookieParser());
-// // app.use(cors());
-// // app.use('/api', router);
-// // app.use(errorMiddleware);
-// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-//   res.status(404);
-// });
-// // mongoose.connection.on('connected', () => {
-// //   console.log('Mongoose is connected!!!!');
-// // });
-// console.log(PORT);
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors());
+// app.use('/api', router);
+// app.use(errorMiddleware);
 
-// export const appInit = async (logger: LoggerService) => {
+// mongoose.connection.on('connected', () => {
+//   console.log('Mongoose is connected!!!!');
+// });
+
+// export const appInit = async () => {
 //   try {
-//     // await mongoose.connect(process.env.DB_URL, {
-//     //   useNewUrlParser: true,
-//     //   useUnifiedTopology: true,
-//     // });
+//     await mongoose.connect(process.env.DB_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
 
 //     app.listen(PORT, () =>
 //       logger.log(`Server started on PORT: ${PORT} http://localhost:${PORT}`)
@@ -54,7 +52,7 @@ export class App {
     exceptionFilter: ExceptionFilter
   ) {
     this.app = express();
-    this.port = Number(process.env.PORT) || 5002;
+    this.port = PORT;
     this.logger = logger;
     this.userController = userController;
     this.exceptionFilter = exceptionFilter;
@@ -72,9 +70,7 @@ export class App {
     this.useRoutes();
     this.useExceptionFilter();
     this.app.listen(PORT, () =>
-      this.logger.log(
-        `Server started on PORT: ${PORT} http://localhost:${PORT}`
-      )
+      this.logger.log(`Server started on http://localhost:${PORT}`)
     );
   }
 }
